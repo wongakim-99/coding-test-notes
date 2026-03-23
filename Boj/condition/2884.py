1d25c65112f5f4006130f29a65cdfaecbbcf222a
김가원(Kimgawon) <202021000@sangmyung.kr>
2026-03-23 15:19:32 +0900

    BOJ - 2884 [알람시계] [refactor]
    - 파이썬 모듈러 연산을 활용한 새로운 풀이 (참고용)
    - 대수적 표현 간결화

--- Boj/condition/2884.py
+++ Boj/condition/2884.py
@@ -23,5 +23,21 @@ def solution() -> Tuple[int, int]:
             return h, m
 
 
+# 2. 조금 더 간결하게 코드 작성 version
+def solution2() -> Tuple[int, int]:
+    h, m = map(int, sys.stdin.readline().split())
+
+    if m >= 45:
+        return h, m - 45
+    else:
+        '''
+        Python 에서 (음수 % 양수)는 항상 양수가 나온다.
+        Python 의 모듈러 연산은 항상 양수 결과를 보장한다.
+        '''
+        h = (h - 1) % 24
+        m = m + 15   # 60 - (45 - m) == m + 15  -> 대수적으로 정리
+        return h, m
+
+
 if __name__ == "__main__":
-    print(*solution())
+    print(*solution2())
